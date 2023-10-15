45e5c909e24f413d3d3493444d9385e7c7c4bf44
vishwaoffl <rvishwavinoth@gmail.com>
2023-10-16 01:08:25 +0530

    #seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,15 @@ import './globals.css'
 const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Store',
-  description: 'Store - The place for all your purchases.',
-}
+  title: 'Dropship - High-Quality Products, Great Deals',
+  description: 'Discover the best place for all your purchases. Your Dropship Store offers a wide range of high-quality products, including electronics, fashion, home decor, and more. Use Dropship® to design a beautiful website store with ease. Start a dropshipping business with only a few clicks! An easy-to-use online store builder trusted by millions of stores. Jumpstart your dropshipping business with us. Start your free trial today. Drop Shipping Integration. Social Media Integration. Unlimited 24/7 support. Full blogging platform.',
+  keywords: 'dropshipping, online store, high-quality products, great deals, electronics, fashion, home decor, Shopify, website builder, dropshipping integration, social media integration, free trial, bangalore, karnataka, tamilnadu, india',
+  author: 'drop ship',
+  image: 'https://res.cloudinary.com/do7ntibpe/image/upload/v1697378708/dropship-coming-soon.jpg',
+  canonical: 'https://dropships.vercel.app',
+};
+
+
 
 export default function RootLayout({
   children,
--- components/ui/currency.tsx
+++ components/ui/currency.tsx
@@ -2,9 +2,9 @@
 
 import { useEffect, useState } from "react";
 
-const formatter = new Intl.NumberFormat('en-US', {
+const formatter = new Intl.NumberFormat('en-IN', {
   style: 'currency',
-  currency: 'USD',
+  currency: 'INR',
 });
 
 interface CurrencyProps {
